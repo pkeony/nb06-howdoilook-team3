5290ecf94e6088739609ca6817c4f85ac5d0a95d
Sunyoung <d2tectiv2@naver.com>
2025-11-11 09:45:53 +0900

    수정: 삭제된 파일 복구

---
+++ src/routers/styleRouter.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import errHandler from '../middlewares/errhandler.js';
+import { getStyles, getStylesList } from '../controllers/styleController.js';
+import { createStyle, updateStyle, deleteStyle } from '../controllers/styleController.js';
+
+const stylesRouter = express.Router();
+
+stylesRouter.get('/', errHandler(getStylesList));
+stylesRouter.get('/:id', errHandler(getStyles));
+
+stylesRouter.post('/', errHandler(createStyle));
+stylesRouter.put('/:styleId', errHandler(updateStyle));
+stylesRouter.delete('/:styleId', errHandler(deleteStyle));
+
+export default stylesRouter;
