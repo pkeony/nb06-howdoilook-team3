ec5fddaebc6d67fb7952de7836563a9fd25754af
flkengh <oys1526@naver.com>
2025-11-18 19:02:58 +0900

    comment 기능 에러 수정하기 이전 버전

---
+++ src/routers/commentRouter.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import {
+  createCommentController,
+  updateCommentController,
+  deleteCommentController
+} from '../controllers/commentController.js';
+
+const router = express.Router();
+
+// 답글 등록 (POST)
+router.post('/curations/:curationId/comments', createCommentController);
+
+// 답글 수정 (PUT)
+router.put('/comments/:commentId', updateCommentController);
+
+// 답글 삭제 (DELETE) ~
+router.delete('/comments/:commentId', deleteCommentController);
+
+export default router;
