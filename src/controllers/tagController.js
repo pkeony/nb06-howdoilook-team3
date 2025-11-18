064e9b063f5b16ea2b57942b01d8bfa60c6066d5
Kelvin Park <pky900824@gmail.com>
2025-11-18 09:21:03 +0900

    Revert "comment 기능 post, put, delete 기능 수정 완료"

--- src/controllers/tagController.js
+++
@@ -1,8 +0,0 @@
-import { getPopularTagsService } from '../services/tagService.js';
-
-// 인기 태그 조회
-// n: 조회하고 싶은 인기 태그 갯수
-export async function getPopularTags(req, res) {
-  const popTagChart = await getPopularTagsService(req.query);
-  res.status(200).send(popTagChart);
-}
