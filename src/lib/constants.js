1f9d899bb6d2c367ba10e94939f8b35a67663736
Kelvin Park <kelvinpark.dev@gmail.com>
2025-11-20 00:05:54 +0900

    배포이미지 마지막

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,4 +2,4 @@ import path from 'path';
 
 export const PORT = process.env.PORT;
 export const PUBLIC_PATH = path.resolve(process.cwd(), 'uploads'); //pc경로 루트디렉토리의 images
-export const STATIC_PATH = '/images'; //url경로
+export const STATIC_PATH = '/uploads'; //url경로
